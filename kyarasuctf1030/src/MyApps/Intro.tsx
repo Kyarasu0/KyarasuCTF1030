9a22340218f1928bae55d07a77fdc252bbb5b48d
Kyarasu0 <Setsuru.lilac@gmail.com>
2024-10-22 01:44:34 +0900

    Intro1

--- kyarasuctf1030/src/MyApps/Intro.tsx
+++ kyarasuctf1030/src/MyApps/Intro.tsx
@@ -1,26 +1,18 @@
+import InvitationInput from '../MyComponents/InvitationInput';
 import './App.css';
 import React from 'react';
 
-function App() {
+function Intro() {
     return (
         <body>
-            <header>
-                <div>
-
-                </div>
-            </header>
             <main>
                 <div>
                     <p>ようこそ！KyarasuCTFへ！</p>
+                    <InvitationInput/>
                 </div>
             </main>
-            <footer>
-                <div>
-
-                </div>
-            </footer>
         </body>
     );
 }
 
-export default App;
+export default Intro;
