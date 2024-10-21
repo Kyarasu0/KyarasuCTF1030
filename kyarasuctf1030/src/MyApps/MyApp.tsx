39c7497eebadef0db484a991bf35d6e3fbb53648
Kyarasu0 <Setsuru.lilac@gmail.com>
2024-10-22 01:32:17 +0900

    Home

---
+++ kyarasuctf1030/src/MyApps/MyApp.tsx
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+    return (
+        <body>
+            <header>
+                <div>
+
+                </div>
+            </header>
+            <main>
+                <div>
+                    <p>ようこそ！KyarasuCTFへ！</p>
+                </div>
+            </main>
+            <footer>
+                <div>
+
+                </div>
+            </footer>
+        </body>
+    );
+}
+
+export default App;
